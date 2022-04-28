d51151a2642d5451fda477c51f9fd54e847658c4
leftover <1614674987@qq.com>
2022-04-28 23:35:26 +0800

    feat: 建立代码规范
    style: 调整格式
    
    	new file:   .eslintignore
    	new file:   .eslintrc.js
    	deleted:    .eslintrc.json
    	new file:   .husky/.gitignore
    	new file:   .husky/commit-msg
    	modified:   .husky/pre-commit
    	new file:   .prettierignore
    	new file:   .prettierrc.js
    	new file:   commitlint.config.js
    	modified:   package-lock.json
    	modified:   package.json

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+  },
+}
