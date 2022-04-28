1793e5043294800f736ec8ce2d3d317ffe66cd53
leftover <1614674987@qq.com>
2022-04-28 23:35:26 +0800

    feat: 建立代码规范

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  eslintIntegration: false,
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  // 使用单引号
+  singleQuote: false,
+  endOfLine: "auto",
+  // 不要分号
+  semi: false,
+  // 箭头函数尽可能不要括号
+  arrowParens: "avoid",
+  rangeStart: 0,
+  rangeEnd: Infinity,
+}
