6203a3b0eabaa1d8d698217e626fa0ec3c0c089b
leftover <1614674987@qq.com>
2022-05-01 16:05:10 +0800

    fix: isExist文件不存在

--- actions/initAction.js
+++ actions/initAction.js
@@ -1,14 +1,16 @@
 const path = require('path')
 const cwd = require('../utils/getCwd')
+const fs = require('fs-extra')
 const { outputJsonSync, readJsonSync } = require('fs-extra')
-const isExist = require('../utils/isExist')
 const configName = require('../config/configName')
-function initAction () {
-  if (isExist(cwd, configName)) {
+function initAction() {
+  if (fs.existsSync(path.resolve(cwd, configName))) {
     throw new Error('配置文件已存在')
   } else {
     // 还需进一步测试
-    const obj = readJsonSync(path.resolve(path.dirname(__dirname), 'config', 'dq.config.default.json'))
+    const obj = readJsonSync(
+      path.resolve(path.dirname(__dirname), 'config', 'dq.config.default.json')
+    )
     outputJsonSync(`./${configName}`, obj, { spaces: 2 })
   }
 }
