ce954a921f3c541eed789a2227857f6c84b2d184
leftover <1614674987@qq.com>
2022-07-19 20:53:37 +0800

    feat: 添加终端logo

--- index.js
+++ index.js
@@ -3,9 +3,20 @@
 const { program } = require('commander')
 const packageJSon = require('./package.json')
 const { BuildCommand, initCommand, createCommand } = require('./command/index')
+const figlet = require('figlet')
+const chalk = require('chalk')
 const version = packageJSon.version
-program.version(version)
+program.version(version).action(() => {})
 initCommand()
 BuildCommand()
 createCommand()
 program.parse(process.argv)
+const logo = chalk.yellow(
+  figlet.textSync('d q - c l i', {
+    horizontalLayout: 'universal smushing',
+    verticalLayout: 'controlled smushing',
+    width: 120,
+    whitespaceBreak: true,
+  })
+)
+console.log(logo)
